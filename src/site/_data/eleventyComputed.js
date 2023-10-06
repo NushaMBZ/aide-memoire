4508db14f4ae5fcec6952bf6a1bba1f37908fc4c
thlurte <thlurte@gmail.com>
2023-10-06 21:13:37 +0530

    20230408151749841563

--- src/site/_data/eleventyComputed.js
+++
@@ -1,9 +0,0 @@
-const { getGraph } = require("../../helpers/linkUtils");
-const { getFileTree } = require("../../helpers/filetreeUtils");
-const { userComputed } = require("../../helpers/userUtils");
-
-module.exports = {
-  graph: (data) => getGraph(data),
-  filetree: (data) => getFileTree(data),
-  userComputed: (data) => userComputed(data),
-};
